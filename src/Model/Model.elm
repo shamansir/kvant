02deb7b391c6de3221c3e8509c1d2568b7b7a442
Ulric Wilfred <shaman.sir@gmail.com>
2019-07-26 19:40:24 +0200

    implement `observe` function draft

--- src/Model/Model.elm
+++ src/Model/Model.elm
@@ -46,9 +46,9 @@ type alias Compatibility =
 
 type alias Weights =
     { values : List Float
-    , logged : List Float
     , sum : Float
-    , sumLogged : Float
+    , raisedToLog : List Float
+    , sumRaisedToLog : Float
     }
 
 
@@ -103,7 +103,8 @@ init size (T t) =
                 <| List.repeat t
                 <| { a = 0, b = 0, c = 0, d = 0 }
         , weights = weights
-        , startingEntropy = logBase weights.sum e - weights.sumLogged / weights.sum
+        , startingEntropy =
+            logBase e weights.sum - weights.sumRaisedToLog / weights.sum
         , entropies = List.repeat count 0
         , sums = sums
         , stack = List.repeat t 0
@@ -115,23 +116,61 @@ init size (T t) =
 weight : List Float -> Weights
 weight values =
     let
-        logged = List.map (\w -> w * logBase w e) values
+        raisedToLog = List.map (\w -> w * logBase e w) values
     in
-        { logged = logged
+        { values = values
         , sum = List.foldl (+) 0 values
-        , sumLogged = List.foldl (+) 0 logged
-        , values = values
+        , raisedToLog = raisedToLog
+        , sumRaisedToLog = List.foldl (+) 0 raisedToLog
         }
 
 
 observe
-    :  Model
+    :  CheckBoundary
+    -> Model
     -> Random.Seed
     -> ( Random.Seed, Observation, Model )
-observe model seed =
+observe checkBoundary model seed =
     let
-        ( min, argmin ) =
-            List.foldl (\w t -> t) ( Random.minInt, -1 ) model.wave
+        findMinimumEntropy ( index, (amount, entropy) ) maybeVals =
+            if checkBoundary { x = 0, y = 0 } then maybeVals
+            else case ( amount, maybeVals ) of
+                ( 0, _ ) -> Nothing
+                ( _, Nothing ) -> Nothing
+                ( nonZeroAmount, Just lastValues ) ->
+                    if nonZeroAmount > 1 && entropy <= lastValues.min then
+                        let
+                            ( noise, nextSeed ) =
+                                Random.step
+                                    (Random.float 0 1 |> Random.map ((*) 0.000001))
+                                    lastValues.seed
+                        in
+                            if entropy + noise < lastValues.min
+                                then
+                                    { lastValues
+                                    | min = entropy + noise
+                                    , indexOfMin = index
+                                    , seed = nextSeed
+                                    } |> Just
+                                else
+                                    { lastValues
+                                    | seed = nextSeed
+                                    } |> Just
+                    else maybeVals
+        maybeIndexOfMin =
+            List.map2
+                Tuple.pair
+                model.sums.ones
+                model.entropies
+                |> List.indexedMap Tuple.pair
+                |> List.foldl
+                    findMinimumEntropy
+                    (Just
+                        { seed = seed
+                        , min = Random.minInt |> toFloat
+                        , indexOfMin = -1
+                        })
+                |> Maybe.map .indexOfMin
     in
         ( seed, Finished, model )
 
@@ -144,12 +183,13 @@ propagate model seed =
     ( seed, model )
 
 
-run : Wave -> Limit -> Model -> Random.Seed -> RunResolution
-run wave (Limit limit) model seed =
+run : Wave -> CheckBoundary -> Limit -> Model -> Random.Seed -> RunResolution
+run wave checkBoundary (Limit limit) model seed =
     let
+
         iterate iterationsLeft ( prevSeed, prevObservation, prevModel ) =
             if iterationsLeft < 0 then ( prevSeed , prevObservation, prevModel )
-            else case observe prevModel prevSeed of
+            else case observe checkBoundary prevModel prevSeed of
                 ( obsSeed, result, observedModel ) ->
                     if result == Continue then
                         case propagate observedModel obsSeed of
