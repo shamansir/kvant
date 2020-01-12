1f5793afaeb464ea89201174274a657ca9a8ed93
Ulric Wilfred <shaman.sir@gmail.com>
2020-01-12 23:47:48 +0100

    the draft of the module responsible for storing history

---
+++ src/WFC/Solver/History.elm
@@ -0,0 +1,57 @@
+module WFC.Solver.History exposing (..)
+
+import List.Zipper as Zipper exposing (..)
+import List.Zipper exposing (Zipper)
+
+
+type alias History a = Zipper a
+
+-- type History a = History Int (Zipper a) -- contains a stack limit z
+
+
+init : a -> History a
+init = Zipper.singleton
+
+
+push : a -> History a -> History a
+push elem history =
+    Zipper.from
+        (Zipper.toList history)
+        elem
+        []
+
+
+last : History a -> a
+last = Zipper.last >> Zipper.current
+
+
+pop : History a -> ( a, Maybe (History a) )
+pop history =
+    let
+        moved = history |> Zipper.last
+    in
+        ( moved |> Zipper.current
+        , moved |> Zipper.before |> Zipper.fromList
+        )
+
+
+back : History a -> History a
+back history =
+    history
+        |> Zipper.previous
+        |> Maybe.withDefault history
+
+
+forward : History a -> History a
+forward history =
+    history
+        |> Zipper.next
+        |> Maybe.withDefault history
+
+
+maybeBack : History a -> Maybe (History a)
+maybeBack = Zipper.previous
+
+
+maybeForward : History a -> Maybe (History a)
+maybeForward = Zipper.next
