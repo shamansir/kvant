fee652c00bf09e843bf9304cfee9826cdba74b9f
Ulric Wilfred <shaman.sir@gmail.com>
2020-01-22 16:23:45 +0100

    we have now learnt how to load images

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
-app.use(express.static('assets'))
+app.use(cors())
+app.use(express.static('./assets'))
 
 app.listen(port, () => console.log(`Serving static files at port ${port}!`))
--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 
 import Browser
+import Http
 
 import Random
 import Task
@@ -11,6 +12,9 @@ import Dict exposing (Dict)
 import Dict as D exposing (map)
 import Array exposing (Array)
 import Array as A exposing (map)
+import Bytes.Decode as Decode exposing (..)
+import Bytes.Decode as Bytes exposing (Decoder)
+import Bytes exposing (Bytes)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -21,11 +25,13 @@ import Render.Flat as Render exposing (..)
 import Render.Text as Render exposing (..)
 import Render.Text as Text exposing (spec)
 import Render.Grid as Render exposing (..)
+import Render.Image as Render exposing (..)
 import Render.Example as Example exposing (..)
 import Render.Example.Flat as FlatExample exposing (..)
 
 import Color exposing (Color)
 import Image exposing (Image)
+import Image.Color as ImageC exposing (toList2d)
 
 import WFC.Core exposing (WFC, TextWFC, TextTracingWFC, TextTracingPlane)
 import WFC.Core as WFC
@@ -45,17 +51,21 @@ import WFC.Solver.History as H exposing (..)
 type alias Model =
     { textExamples : List TextExample
     , imageExamples : List ImageExample
+    , images : Dict String Image
     }
 
 
 type ExampleKind
-    = Text
-    | Image
+    = TextExample
+    | ImageExample
 
+
+type alias Url = String
 
 type Msg
     = NoOp
     | WithExample ExampleKind ExampleId ExampleMsg
+    | GotImage Url (Result Http.Error Image)
 
 
 textOptions : WFC.Options Vec2
@@ -113,6 +123,7 @@ init =
                 (4, 4)
             ]
         , imageExamples = []
+        , images = Dict.empty
         }
 
 
@@ -156,14 +167,26 @@ update msg model =
             in
 
                 case kind of
-                    Text ->
+                    TextExample ->
                         injectUpdates
                             model.textExamples
                             (\newExamples -> { model | textExamples = newExamples })
-                    Image ->
+                    ImageExample ->
                         injectUpdates
                             model.imageExamples
                             (\newExamples -> { model | imageExamples = newExamples })
+
+        GotImage url result ->
+            ( case result of
+                Ok image ->
+                    { model
+                    | images =
+                        model.images
+                            |> Dict.insert url image
+                    }
+                Err _ -> model
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
@@ -172,19 +195,36 @@ view model =
         textRenderer =
             FlatExample.make Text.toGrid Text.spec
         viewTextExample index =
-            Example.view (WithExample Text index) textRenderer
+            Example.view (WithExample TextExample index) textRenderer
         viewImageExample index _ = div [] [] -- FIXME: implement
+        viewImage index ( url, image ) =
+            div
+                []
+                [ text <| String.fromInt index ++ ". " ++ url
+                , Render.grid Render.pixel <| ImageC.toList2d image
+                ]
     in
         div
             [ ]
-            <| (model.textExamples |> List.indexedMap viewTextExample)
+            <| (model.images |> Dict.toList |> List.indexedMap viewImage)
+            ++ (model.textExamples |> List.indexedMap viewTextExample)
             ++ (model.imageExamples |> List.indexedMap viewImageExample)
 
 
 main : Program {} Model Msg
 main =
     Browser.application
-        { init = \_ _ _ -> ( init, Cmd.none )
+        { init =
+            \_ _ _ ->
+                ( init
+                , Http.get
+                    { url = "http://localhost:3000/samples/Cat.png"
+                    , expect =
+                        Http.expectBytesResponse
+                            (GotImage "/Cat.png")
+                            loadImage
+                    }
+                )
         , onUrlChange = always NoOp
         , onUrlRequest = always NoOp
         , subscriptions = always Sub.none
@@ -193,3 +233,25 @@ main =
         }
 
 
+loadImage : Http.Response Bytes -> Result Http.Error (Image)
+loadImage response =
+    case response of
+        Http.BadUrl_ url ->
+          Err (Http.BadUrl url)
+
+        Http.Timeout_ ->
+          Err Http.Timeout
+
+        Http.NetworkError_ ->
+          Err Http.NetworkError
+
+        Http.BadStatus_ metadata _ ->
+          Err (Http.BadStatus metadata.statusCode)
+
+        Http.GoodStatus_ metadata bytes ->
+          case Image.decode bytes of
+            Just image ->
+              Ok image
+
+            Nothing ->
+              Err (Http.BadBody "Image failed to be decoded")
