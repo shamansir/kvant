012a9344c8157cf2e3ae14b58564573bd6172130
Ulric Wilfred <shaman.sir@gmail.com>
2020-12-25 14:10:39 +0100

    add Truchet example with the fake random values generation (to replace with Kvant later)

---
+++ example/truchet/Truchet.elm
@@ -0,0 +1,68 @@
+port module Truchet exposing (..)
+
+
+import Array exposing (Array)
+import Browser
+import Random
+import Html exposing (div)
+
+
+type alias TilesCount = Int
+
+
+type alias AdjacencyRules = Array ()
+
+
+type alias OutputSize = (Int, Int)
+
+
+type alias Output = List (List Int)
+
+
+type Msg
+    = Calculate TilesCount AdjacencyRules OutputSize
+    | SendOutput Output
+
+
+type alias Model = ()
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg _ =
+    ( ()
+    , case msg of
+        Calculate count _ ( sizeX, sizeY ) ->
+            Random.list sizeY (Random.list sizeX <| Random.int 0 <| count - 1)
+                |> Random.generate SendOutput
+        SendOutput output ->
+            sendOutput output
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    triggerCalculation <|
+        \{ count, rules, size } ->
+            Calculate count rules size
+
+
+main : Program {} Model Msg
+main =
+    Browser.element
+        { init = always ( (), Cmd.none )
+        , subscriptions = subscriptions
+        , update = update
+        , view = always <| div [] []
+        }
+
+
+port sendOutput : Output -> Cmd msg
+
+
+port triggerCalculation :
+    (
+        { count : TilesCount
+        , rules : AdjacencyRules
+        , size : OutputSize
+        } -> msg
+    ) -> Sub msg
