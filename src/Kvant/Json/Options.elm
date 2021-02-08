fc168171e33484e1f1d2373ddecb6286627b8ed0
Ulric Wilfred <shaman.sir@gmail.com>
2021-02-08 21:57:01 +0100

    split options to `preprocessing` options (`Overlapping`) and output options

--- src/Kvant/Json/Options.elm
+++ src/Kvant/Json/Options.elm
@@ -1,6 +1,6 @@
 module Kvant.Json.Options exposing (..)
 
-import Kvant.Solver.Options exposing (..)
+import Kvant.Solver.Options as Options
 import Kvant.Plane exposing (Plane(..), Boundary(..), Symmetry(..))
 
 
@@ -8,55 +8,44 @@ import Json.Decode as D
 import Json.Encode as E
 
 
-decode : D.Decoder Options
-decode =
-    D.map4
-
-        (\approach outputBoundary otputWidth outputHeight ->
-            Options approach outputBoundary ( otputWidth, outputHeight )
+decodeOverlappingOptions : D.Decoder Options.Overlapping
+decodeOverlappingOptions =
+    D.map3
+        (\psize iboundary symmetry ->
+            { patternSize = psize
+            , boundary = iboundary
+            , symmetry = symmetry
+            }
         )
-
-        (D.field "approach" D.string
+        (D.field "patternSize" <| D.map (\n -> (n, n)) <| D.int)
+        (D.field "inputBoundary" D.string
             |> D.andThen
-                (\approach ->
-                    case approach of
-
-                        "overlapping" ->
-                            D.map3
-                                (\psize iboundary symmetry ->
-                                    Overlapping
-                                        { patternSize = psize
-                                        , inputBoundary = iboundary
-                                        , symmetry = symmetry
-                                        }
-                                )
-                                (D.field "patternSize" <| D.map (\n -> (n, n)) <| D.int)
-                                (D.field "inputBoundary" D.string
-                                    |> D.andThen
-                                        (\boundary ->
-                                            case boundary of
-                                                "bounded" -> D.succeed Bounded
-                                                "periodic" -> D.succeed Periodic
-                                                _ -> D.fail <| "Invalid boundary: " ++ boundary
-                                        )
-                                )
-                                (D.field "symmetry" D.string
-                                    |> D.andThen
-                                        (\symmetry ->
-                                            case symmetry of
-                                                "none" -> D.succeed NoSymmetry
-                                                "flip-only" -> D.succeed FlipOnly
-                                                "rotate-only" -> D.succeed RotateOnly
-                                                "flip-and-rotate" -> D.succeed FlipAndRotate
-                                                _ -> D.fail <| "Invalid symmetry: " ++ symmetry
-                                        )
-                                )
-
-                        "tiled" -> D.succeed Tiled
-                        _ -> D.fail <| "Invalid approach: " ++ approach
+                (\boundary ->
+                    case boundary of
+                        "bounded" -> D.succeed Bounded
+                        "periodic" -> D.succeed Periodic
+                        _ -> D.fail <| "Invalid boundary: " ++ boundary
+                )
+        )
+        (D.field "symmetry" D.string
+            |> D.andThen
+                (\symmetry ->
+                    case symmetry of
+                        "none" -> D.succeed NoSymmetry
+                        "flip-only" -> D.succeed FlipOnly
+                        "rotate-only" -> D.succeed RotateOnly
+                        "flip-and-rotate" -> D.succeed FlipAndRotate
+                        _ -> D.fail <| "Invalid symmetry: " ++ symmetry
                 )
         )
 
+
+decodeOutputOptions : D.Decoder Options.Output
+decodeOutputOptions =
+    D.map3
+        (\oboundary width height ->
+            ( oboundary, ( width, height ) )
+        )
         (D.field "outputBoundary" D.string
             |> D.andThen (\boundary ->
                 case boundary of
@@ -65,70 +54,64 @@ decode =
                     _ -> D.fail <| "Invalid boundary: " ++ boundary
             )
         )
-
         (D.field "outputWidth" D.int)
-
         (D.field "outputHeight" D.int)
 
 
-encode : Options -> E.Value
-encode opts =
-    E.object <|
+encodeOverlappingOptions : Options.Overlapping -> E.Value
+encodeOverlappingOptions opts =
+    E.object
         [
-            ( "approach"
-            , E.string <|
-                case opts.approach of
-                    Overlapping _ -> "overlapping"
-                    Tiled -> "tiled"
+
+            ( "patternSize"
+            , E.int <|
+                case opts.patternSize of
+                    ( n, _ ) -> n
             )
         ,
+
+            ( "inputBoundary"
+            , E.string <|
+                case opts.boundary of
+                    Bounded -> "bounded"
+                    Periodic -> "periodic"
+            )
+        ,
+            ( "symmetry"
+            , E.string <|
+                case opts.symmetry of
+                    NoSymmetry -> "none"
+                    FlipOnly -> "flip-only"
+                    RotateOnly -> "rotate-only"
+                    FlipAndRotate -> "flip-and-rotate"
+            )
+
+        ]
+
+
+encodeOutputOptions : Options.Output -> E.Value
+encodeOutputOptions ( boundary, size ) =
+    E.object
+        [
             ( "outputBoundary"
             , E.string <|
-                case opts.outputBoundary of
+                case boundary of
                     Bounded -> "bounded"
                     Periodic -> "periodic"
             )
         ,
             ( "outputWidth"
             , E.int <|
-                case opts.outputSize of
+                case size of
                     ( width, _ ) -> width
             )
         ,
             ( "outputHeight"
             , E.int <|
-                case opts.outputSize of
+                case size of
                     ( _, height ) -> height
             )
-        ] ++
+        ]
 
-        case opts.approach of
-            Overlapping oopts ->
-                [
 
-                    ( "patternSize"
-                    , E.int <|
-                        case oopts.patternSize of
-                            ( n, _ ) -> n
-                    )
-                ,
 
-                    ( "inputBoundary"
-                    , E.string <|
-                        case oopts.inputBoundary of
-                            Bounded -> "bounded"
-                            Periodic -> "periodic"
-                    )
-                ,
-                    ( "symmetry"
-                    , E.string <|
-                        case oopts.symmetry of
-                            NoSymmetry -> "none"
-                            FlipOnly -> "flip-only"
-                            RotateOnly -> "rotate-only"
-                            FlipAndRotate -> "flip-and-rotate"
-                    )
-
-                ]
-            Tiled ->
-                []
